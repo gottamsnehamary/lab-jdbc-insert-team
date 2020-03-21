a914fd188dccce5ad0d62dbe11da426f45df6171
gottamsnehamary <gottamsneha21@gmail.com>
2020-03-21 22:16:26 +0530

    lab-jdbc-insert-team

--- src/model/Team.java
+++ src/model/Team.java
@@ -1,1 +1,33 @@
+package model;
 
+class team{
+	String name;
+	String coach;
+	String captain;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCoach() {
+		return coach;
+	}
+	public void setCoach(String coach) {
+		this.coach = coach;
+	}
+	public String getCaptain() {
+		return captain;
+	}
+	public void setCaptain(String captain) {
+		this.captain = captain;
+	}
+	public team(String name, String coach, String captain) {
+		super();
+		this.name = name;
+		this.coach = coach;
+		this.captain = captain;
+	}
+	
+	
+}
